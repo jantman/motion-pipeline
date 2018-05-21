f10a409e149796f948137748f7b0fb643d7ddadc
Jason Antman <jason@jasonantman.com>
2018-05-21 06:45:14 -0400

    add interactive database shell entrypoint

---
+++ motion_pipeline/database/dbshell.py
@@ -0,0 +1,71 @@
+"""
+The latest version of this package is available at:
+<http://github.com/jantman/motion-pipeline>
+
+##################################################################################
+Copyright 2018 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+
+    This file is part of motion-pipeline, also known as motion-pipeline.
+
+    motion-pipeline is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    motion-pipeline is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with motion-pipeline.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+##################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/motion-pipeline> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+##################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+##################################################################################
+"""
+
+import atexit
+import logging
+import code
+
+from motion_pipeline.database.db import init_db, cleanup_db, db_session
+from motion_pipeline.cliutils import set_log_debug, set_log_info
+from motion_pipeline.database.models import *  # noqa
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    global logger
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s %(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger()
+    set_log_info(logger)
+
+    logger.info('Initializing DB...')
+    init_db()
+    atexit.register(cleanup_db)
+    logger.info('Done initializing database')
+
+    # enter REPL
+    print(
+        'All models are imported; database session is available as "db_session"'
+    )
+    code.interact(local=locals())
+
+
+if __name__ == "__main__":
+    main()

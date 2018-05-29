3d23facfed46e055f88bc0cffe8ff66cd5d38e10
Jason Antman <jason@jasonantman.com>
2018-05-29 05:51:24 -0400

    update example settings

--- motion_pipeline/settings_example.py
+++ motion_pipeline/settings_example.py
@@ -2,7 +2,8 @@
 # Redis Broker Settings #
 #########################
 
-REDIS_BROKER_URL = 'redis://localhost:6379/0'
+REDIS_BROKER_URL = 'redis://192.168.0.24:6379/0'
+SIMPLECACHE_KEY_PREFIX = 'simplecache/'
 
 #####################
 # Database Settings #
@@ -23,8 +24,33 @@ BUCKET_PREFIX = 'motion/'
 #: as None. When using S3, credentials will be retrieved using the normal
 #: cross-SDK methods used by boto3, and the MINIO key settings are ignored.
 MINIO_URL = 'http://192.168.0.24:9000'
-MINIO_ACCESS_KEY = 'abcd'
-MINIO_SECRET_KEY = '123abc456'
+MINIO_ACCESS_KEY = 'zzzzzzzzzzzzzzzzzzzz'
+MINIO_SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+#: If the minio storage directory is reachable from the web and worker hosts
+#: or containers, set it here and files from minio will be read directly from
+#: this path (as the bucket root) instead of downloaded. Otherwise, set to None.
+MINIO_LOCAL_MOUNTPOINT = '/mnt/minio/some_name/motion'
+
+#############################
+# Image Processing Settings #
+#############################
+
+THUMBNAIL_MAX_SIZE = 640, 640
+
+#########################
+# Notification Settings #
+#########################
+
+PUSHOVER_API_KEY = 'PushOverKeyXxxxxxxxxxxxxxxxxxx'
+PUSHOVER_USER_KEY = 'uXxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+PUSHOVER_SEND_EMERGENCY = True
+PUSHOVER_RETRY = True
+
+#: The base URL to use for links to the web UI in notifications. Specified
+#: manually here in case you're running on a dynamic IP, NATted, etc.
+#: This must end in a trailing slash.
+NOTIFICATION_BASE_URL = 'http://foo.example.com/'
 
 ###############################
 # motion_handler.py settings #
@@ -35,3 +61,51 @@ MOTION_SAVE_DIR = '/var/lib/motion'
 HANDLER_LOG_INFO = True
 HANDLER_LOG_DEBUG = False
 HANDLER_MAX_UPLOAD_ATTEMPTS = 4
+
+##############################
+# Camera settings #
+##############################
+
+CAM_USER = 'MyUser'
+CAM_PASS = 'MyPass'
+
+CAMERAS = {
+    'CAM1': {
+        'mjpeg_url': '/cam1/cgi-bin/mjpg/video.cgi?channel=1&subtype=1',
+        'snapshot_url': '/cam1/cgi-bin/snapshot.cgi?channel=0',
+        'rstp_url': 'rtsp://%s:%s@192.168.0.61:80/cam/realmonitor?channel=1&subtype=0&proto=Dahua3' % (CAM_USER, CAM_PASS),
+        'control_info': {
+            'control_type': 'Amcrest',
+            'control_user': CAM_USER,
+            'control_pass': CAM_PASS,
+            'control_base_url': 'http://192.168.0.61:80/',
+            'control_presets': {
+                '1': 'One',
+                '2': 'Two',
+                '3': 'Three',
+                '4': 'Four',
+                '5': '5',
+                '6': '6',
+                '7': '7',
+                '8': '8',
+                '9': '9'
+            }
+        },
+        'motion_host': '192.168.0.24',
+        'motion_port': 8080,
+        'motion_camera_id': 1
+    },
+    'CAM2': {
+        'mjpeg_url': '/cam2/cgi-bin/mjpg/video.cgi?channel=1&subtype=1',
+        'snapshot_url': '/cam2/cgi-bin/snapshot.cgi?channel=0',
+        'rstp_url': 'rtsp://%s:%s@192.168.0.63:80/cam/realmonitor?channel=1&subtype=0&proto=Dahua3' % (CAM_USER, CAM_PASS),
+        'control_info': None,
+        'motion_host': '192.168.0.36',
+        'motion_port': 8080,
+        'motion_camera_id': 2
+    }
+}
+
+# For development of the live view, uncommenting these makes it easier...
+# CAMERAS['CAM1']['mjpeg_url'] = '/static/640x480.png'
+# CAMERAS['CAM2']['mjpeg_url'] = '/static/640x480.png'
